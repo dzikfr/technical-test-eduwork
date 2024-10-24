c73dd56dce3d27cb762671ecefcbb450cd7c5c5b
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-10-24 18:51:12 +0700

    improve dashboard admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Admin = () => {
   return (
     <div>
-      <Navbar/>
-    </div>
-  )
-}
+      <Navbar />
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 min-h-screen p-4">
+        <div className="border border-gray-300 p-4 flex items-center justify-center">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Product</h2>
+            <div className="card-actions">
+              <Link to={"/admin/product"} className="btn">
+                Go
+              </Link>
+            </div>
+          </div>
+        </div>
 
-export default Admin
+        <div className="border border-gray-300 p-4 flex items-center justify-center">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Category</h2>
+            <div className="card-actions">
+              <Link to={"/admin/category"} className="btn">
+                Go
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="border border-gray-300 p-4 flex items-center justify-center">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Order</h2>
+            <div className="card-actions">
+              <Link to={"/admin/order"} className="btn">
+                Go
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="border border-gray-300 p-4 flex items-center justify-center">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">User</h2>
+            <div className="card-actions">
+              <Link to={"/admin/user"} className="btn">
+                Go
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
